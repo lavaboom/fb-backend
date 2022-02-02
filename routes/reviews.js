db22fac1d374016354c9c0cb7b0c101844666a85
Smoothie <lavaboom@yahoo.com>
2022-02-01 21:49:24 -0500

    will do rating calculation from frontend instead

--- routes/reviews.js
+++ routes/reviews.js
@@ -9,6 +9,17 @@ Application logic
 POST   /api/reviews/
 ----------------------------------------------------------------------------- */
 
+// GET /api/reviews/:userID
+// Fetch all reviews of a user
+// Expects valid JWT authentication to run through the 'authenticate' middleware
+router.get('/:userID/all-reviews', authenticate, (req, res) => {
+    knex('reviews')
+        .where({ for_user_id: req.params.userID })
+        .then((reviews) => {
+            res.status(200).json(reviews);
+        });
+});
+
 // POST /api/reviews/:tripID/:driverID
 // Post a review
 router.post('/:tripID/:driverID', authenticate, (req, res) => {
